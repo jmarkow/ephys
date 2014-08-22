487b342e50e9789751f8ea5591f453b7722987e1
Jeff Markowitz <markowitzmeister@gmail.com>
2014-08-22 09:37:08 -0400

    Bugfix

--- helpers/frontend/frontend_dataextract.m
+++ helpers/frontend/frontend_dataextract.m
@@ -31,7 +31,7 @@ end
 sonogram_im=sonogram_im(startidx:stopidx,:);
 sonogram_im=flipdim(sonogram_im,1);
 [f,t]=size(sonogram_im);
-im_son_to_vec=(length(DATA.audio.norm_data)-350)/t;
+im_son_to_vec=(length(DATA.audio.norm_data)-500)/t;
 
 data_types={'ephys','ttl','digout','digin','adc','aux','audio','playback'};
 
@@ -71,10 +71,12 @@ for i=1:size(EXT_PTS,1)
 
 	chunk_sonogram_im=chunk_sonogram_im(startidx:stopidx,:);
 	chunk_sonogram_im=flipdim(chunk_sonogram_im,1);
+	[f,t]=size(chunk_sonogram_im);
+	chunk_im_son_to_vec=(length(EXTDATA.audio.data)-500)/t;
 
 	if ~isempty(EXTDATA.ttl.data)
 		ttl_points=find(EXTDATA.ttl.data>.5);
-		ttl_son=round(ttl_points/im_son_to_vec);
+		ttl_son=round(ttl_points/chunk_im_son_to_vec);
 		ttl_son(ttl_son<1|ttl_son>size(chunk_sonogram_im,2))=[];
 		chunk_sonogram_im(1:10,round(ttl_son))=62;
 	end
